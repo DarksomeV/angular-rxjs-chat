9b8cf660f3dc4cc82c33f980c60bfba75a6c3a16
oleg <oleglevenchuk32@gmail.com>
2020-02-23 11:42:30 +0200

    finished message sevice and added tests

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -3,7 +3,7 @@ import {Observable, Subject} from 'rxjs';
 import {Message} from './message.model';
 import {User} from '../user/user.model';
 import {Thread} from '../thread/thread.model';
-import {filter, publishReplay, refCount, scan} from 'rxjs/operators';
+import {filter, map, publishReplay, refCount, scan, tap} from 'rxjs/operators';
 
 const initialMessages: Message[] = [];
 
@@ -25,14 +25,41 @@ export class MessagesService {
   // it's a way we can perform changes on *all* messages (that are currently // stored in `messages`)
   updates: Subject<any> = new Subject<any>();
 
+  // action streams
+  create: Subject<Message> = new Subject<Message>();
+  markThreadAsRead: Subject<any> = new Subject<any>();
+
+
 
   constructor() {
     this.messages = this.updates.pipe(
+      tap((res) => console.log('updates', res)),
       scan((messages: Message[], operation: IMessagesOperation) => operation(messages), initialMessages),
       publishReplay(1),
       refCount()
     );
 
+    this.create.pipe(
+      map( (message: Message): IMessagesOperation => (messages: Message[]) => messages.concat(message) )
+    ).subscribe(this.updates);
+
+    this.newMessages.subscribe(this.create);
+
+    // similarly, `markThreadAsRead` takes a Thread and then puts an operation // on the `updates` stream to mark the Messages as read
+    this.markThreadAsRead.pipe(
+      map((thread: Thread) => {
+      return (messages: Message[]) => {
+        return messages.map((message: Message) => {
+          // note that we're manipulating `message` directly here. Mutability
+          // can be confusing and there are lots of reasons why you might want
+          // to, say, copy the Message object or some other 'immutable' here
+          if (message.thread.id === thread.id) {
+            message.isRead = true;
+          }
+          return message;
+        });
+      };
+    })).subscribe(this.updates);
   }
 
   addMessage(message: Message): void {
